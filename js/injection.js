81536d8c6257cdfa2add5b724c4463d70dbd598c
Dmytro Bazas <babyx64@gmail.com>
2015-05-11 04:53:47 +0300

    added transcription in exporting, minor fix in indexing

--- js/injection.js
+++ js/injection.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
     $('.dict-filter.dict-actions').find('.last-btn').before('<a class="export" role="button"><button data-tooltip="Export"><i style="display: inline-block; background: url(https://raw.githubusercontent.com/bilabon/Export-leo/master/export-leo-16.png) no-repeat; width: 16px; height: 16px;"></i></button></a>');
 
     $(".export").click(function () {
-        var $table = $('div.dict-item-word.checked').find('.item-word-translate');
-        function exportTableToCSV($table, filename) {
-
+        function exportTableToCSV(table, filename) {
             var colDelim = '","', rowDelim = '"\r\n', csv = '';
-            $table.each(function(index) {
+
+            table.each(function(index) {
+                index += 1;
+                transcription = $(this).parent().find('.item-word-sound').data('tooltip');
                 var eng = $.trim($(this).find('b').text());
                 var rus = $.trim($(this).find('span').text());
-                csv += '"' + index + colDelim + eng + colDelim + rus + rowDelim;
+                csv += '"' + index + colDelim + eng + colDelim + transcription + colDelim + rus + rowDelim;
             });
 
             var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
@@ -20,6 +21,7 @@ $(document).ready(function () {
                 'href': csvData
             });
         }
-        exportTableToCSV.apply(this, [$table, 'lingualeo.csv']);
+        var table = $('div.dict-item-word.checked').find('.item-word-translate');
+        exportTableToCSV.apply(this, [table, 'lingualeo.csv']);
     });
 });
